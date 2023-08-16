491aa2e8d7bdbd5147fbad8cd7d6afea77a86d70
jurecapuder <capuder.j@gmail.com>
2023-08-16 23:55:43 +0200

    Adding navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav
+      className={`
+        ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        
+      </div>
+    </nav>
   )
 }
 
