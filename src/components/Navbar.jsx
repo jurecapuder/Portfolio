8e19fb425c8f6486b4994a94d097ac1c6b6590b9
jurecapuder <capuder.j@gmail.com>
2023-08-17 19:55:33 +0200

    Fixing <Link> bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
-  const [active, setActive] = useState(' ');
+const [active, setActive] = useState("")
 
   return (
     <nav
