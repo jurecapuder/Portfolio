290873b0175f3b3c72b3d5b2b6de26b864de04dc
jurecapuder <capuder.j@gmail.com>
2023-08-17 18:36:38 +0200

    Updating files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,23 @@ import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
+  const [active, setActive] = useState(' ');
+
   return (
     <nav
-      className={`
-        ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        
+        <Link
+          to="/"
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+        </Link>
       </div>
     </nav>
   )
