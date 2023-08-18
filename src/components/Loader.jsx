ec08f9add272c3ec7a65e486d0c1b387520a440e
jurecapuder <capuder.j@gmail.com>
2023-08-19 00:41:46 +0200

    Adding loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei"
 
 const Loader = () => {
+  const { progress } = useProgress();
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: '#f1f1f1',
+          fontWeight: 800,
+          marginTop: 40
+        }}
+      >{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
