5f8994dfc6267076d4617430fafcb3313e628296
jurecapuder <capuder.j@gmail.com>
2023-08-18 21:09:11 +0200

    Finishing up navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav
@@ -16,10 +17,13 @@ const Navbar = () => {
         <Link
           to="/"
           className='flex items-center gap-2'
-          onClick={() => setActive("")}
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'> Jure <span className='sm:block hidden'>| Capuder</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Jure &nbsp; <span className='sm:block hidden'>| Capuder</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
@@ -40,6 +44,41 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.id
+                      ? "text-white"
+                      : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                >
+                  <Link
+                    to={`#${link.id}`}
+                    onClick={() => {
+                      setActive(link.id);
+                      setToggle(!toggle);
+                    }}
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
       </div>
     </nav>
   )
