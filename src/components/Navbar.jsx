419b6985d1bff88902c55d1ca6a232d531a809d4
jurecapuder <capuder.j@gmail.com>
2023-08-18 18:08:35 +0200

    Adding postcss and autoprefixer + updating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,7 @@ const [active, setActive] = useState("")
           }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'> Jure <span className='sm:block hidden'>| Capuder</span></p>
         </Link>
       </div>
     </nav>
