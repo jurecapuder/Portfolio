6e01e15c8c17f88a1d9dffc8e86da451af207a52
jurecapuder <capuder.j@gmail.com>
2023-08-19 19:36:15 +0200

    Fixing spelling error

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -55,7 +55,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov:25 }}
       gl={{ preserveDrawingBuffer: true }}
