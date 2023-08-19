f267c3dc1a411e86e3bf2fac07ee9be27ed78a77
jurecapuder <capuder.j@gmail.com>
2023-08-19 17:16:01 +0200

    Updating section wrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,11 +1,20 @@
-import { motion } from "framer-motion";
+import { motion, stagger } from "framer-motion";
 
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) => function HOC() {
   return(
-    <motion.section>
+    <motion.section
+      variants={staggerContainer()}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: true, amount: 0.25 }}
+      className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+    >
+      <span className="hash-span" id={idName}>
+        &nbsp;
+      </span>
       <Component />
     </motion.section>
   )
