2409b8d7fde624694bc545b1b34c3f186a16e2da
jurecapuder <capuder.j@gmail.com>
2023-08-20 21:11:10 +0200

    Fixing spelling error

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -51,7 +51,7 @@ const Feedbacks = () => {
     <div className="mt-12 bg-black-100 rounded-[20px]">
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
         <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>What other say</p>
+          <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
