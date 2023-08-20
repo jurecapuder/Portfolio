ca63bc5b6137b33eafa42bd7dda2a0a2d5f979f4
jurecapuder <capuder.j@gmail.com>
2023-08-20 22:57:56 +0200

    Adding earth object

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -78,6 +78,13 @@ const Contact = () => {
           </button>
         </form>
       </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
     </div>
   )
 }
