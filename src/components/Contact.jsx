86eb0ef124ca5aaa22730761a55357c23746ca4d
jurecapuder <capuder.j@gmail.com>
2023-08-20 21:13:37 +0200

    Adding imports to contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from '@emailjs/browser';
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
   return (
