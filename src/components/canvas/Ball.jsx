3230fe8348f4beec6a957be481bbe0221a0394f6
jurecapuder <capuder.j@gmail.com>
2023-08-21 14:27:02 +0200

    Fixing bugs

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -33,7 +33,7 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
