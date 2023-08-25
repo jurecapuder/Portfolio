9276fe6ca233c0ae15c7c27b1c8baca345e4ac81
jurecapuder <capuder.j@gmail.com>
2023-08-25 21:23:26 +0200

    Fixing warning

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,7 @@ import * as random from 'maath/random/dist/maath-random.esm';
 const Stars = (props) => {
   const ref = useRef();
 
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  const sphere = random.inSphere(new Float32Array(6000), { radius: 1.2 })
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
