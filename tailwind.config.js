415ce2ed05217271ede52c738b8162c502c3e58d
jurecapuder <capuder.j@gmail.com>
2023-10-26 17:37:36 +0200

    Updating paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/misc/herobg.png')",
       },
     },
   },
