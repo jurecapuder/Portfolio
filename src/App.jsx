785ded8810035c2f01879ce9297df125171978d8
jurecapuder <capuder.j@gmail.com>
2023-10-30 23:08:20 +0100

    Updating app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import { BrowserRouter } from "react-router-dom";
-
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Footer } from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Footer, ResumeViewer } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
-        <Footer />
+        <Routes>
+          <Route path="/resume" element={<ResumeViewer />} />
+          <Route path="/*" element={
+            <>
+              <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                <Navbar />
+                <Hero />
+              </div>
+              <About />
+              <Experience />
+              <Tech />
+              <Works />
+              <Feedbacks />
+              <div className='relative z-0'>
+                <Contact />
+                <StarsCanvas />
+              </div>
+              <Footer />
+            </>
+          }/>
+        </Routes>
       </div>
     </BrowserRouter>
   );
