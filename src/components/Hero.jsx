f8d0813322bd272ff15b501d5368c1ece6fe8149
jurecapuder <capuder.j@gmail.com>
2023-10-30 21:29:23 +0100

    Updating hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}> Hi, I'm <span className='text-[#915EFF]'>Jure</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}> Hi, I&apos;m <span className='text-[#915EFF]'>Jure</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />interfaces and web applications.
           </p>
