4687eb8fcdf6d766ce7dc55266a4d7753a941e24
jurecapuder <capuder.j@gmail.com>
2023-10-30 22:27:36 +0100

    Creating resume viewer component

---
+++ src/components/ResumeViewer.jsx
@@ -0,0 +1,11 @@
+import pdfFile from "../assets/misc/CV - Jure Capuder.pdf";
+
+const ResumeViewer = () => {
+  return (
+    <object data={pdfFile} type="application/pdf" width="100%" height="100%">
+      <p>It appears you don&apos;t have a PDF plugin for this browser. You can <a href={pdfFile}>click here to download the PDF file.</a></p>
+    </object>
+  );
+};
+
+export default ResumeViewer;
