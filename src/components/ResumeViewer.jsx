65a737943735f9c590cec24555fcf66743236ce6
jurecapuder <capuder.j@gmail.com>
2023-10-31 14:12:53 +0100

    Updating resume viewer

--- src/components/ResumeViewer.jsx
+++ src/components/ResumeViewer.jsx
@@ -1,10 +1,33 @@
+import { useNavigate } from 'react-router-dom';
 import pdfFile from "../assets/misc/CV - Jure Capuder.pdf";
 
 const ResumeViewer = () => {
+  const navigate = useNavigate();
+
+  const downloadPdf = () => {
+    window.open(pdfFile);
+  };
+
+  const goBack = () => {
+    navigate('/');
+  };
+
   return (
-    <object data={pdfFile} type="application/pdf" style={{ height: '100vh', width: '100%' }}>
-      <p>It appears you don&apos;t have a PDF plugin for this browser. You can <a href={pdfFile}>click here to download the PDF file.</a></p>
-    </object>
+    <div>
+      <object data={pdfFile} type="application/pdf" style={{ height: '100vh', width: '100%' }}>
+        <div style={{ fontSize: '16px', padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          Click here to download the PDF file.
+        </div>
+
+        <div style={{ fontSize: '24px', padding: '20px', fontWeight: 'bold', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <button onClick={downloadPdf} style={{ color: 'black', background: 'lightblue', padding: '10px', border: 'none', borderRadius: '5px', marginTop: '10px' }}>Download</button>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '65vh'}}>
+          <button onClick={goBack} style={{ color: 'black', background: 'lightblue', padding: '10px', border: 'none', borderRadius: '5px' }}>Back</button>
+        </div>
+      </object>
+    </div>
   );
 };
 
