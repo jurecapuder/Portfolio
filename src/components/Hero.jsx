28efd3ddd013a654293d6159acd02660934e33a2
jurecapuder <capuder.j@gmail.com>
2024-02-22 20:47:20 +0100

    Update Hero component styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,13 +26,13 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-1 sm:h-40 h-20 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}> Hi, I&apos;m <span className='text-[#915EFF]'>Jure</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />interfaces and web applications.
+            Self-taught Full-stack <br className='block sm:hidden' /> Web Developer
           </p>
         </div>
       </div>
